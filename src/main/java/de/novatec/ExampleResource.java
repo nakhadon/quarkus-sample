39e2c83d3097b5434439daad761cb4473df86644
Donsuypae Nakharin <nd@novatec-gmbh.de>
2020-07-07 13:11:19 +0200

    Added uuid sample

--- src/main/java/de/novatec/ExampleResource.java
+++ src/main/java/de/novatec/ExampleResource.java
@@ -1,16 +1,20 @@
 package de.novatec;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
-@Path("/hello")
+@Path("/sample")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Path("/{id}")
+    public String hello(@PathParam UUID id) {
+        return id.toString();
     }
 }
